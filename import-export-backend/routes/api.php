a57be866a8d1252af5c8640c1f1c8b7feea0a788
sirajul83 <sirajulislam8354@gmail.com>
2022-09-09 22:28:17 +0600

    web route and web route modified

--- import-export-backend/routes/api.php
+++ import-export-backend/routes/api.php
@@ -3,16 +3,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ImportExportController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
--- import-export-backend/routes/web.php
+++ import-export-backend/routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImportExportController;
 
 
-// Route::get('/', [ImportExportController::class, 'index']);
-// Route::get('import', [ImportExportController::class, 'import'])->name('import');
-// Route::post('upload', [ImportExportController::class, 'upload'])->name('import.upload');
+Route::get('/', [ImportExportController::class, 'index']);
+Route::get('import', [ImportExportController::class, 'import'])->name('import');
+Route::post('upload', [ImportExportController::class, 'upload'])->name('import.upload');
 
-// Route::get('batch', [ImportExportController::class, 'batch'])->name('batch');
+Route::get('batch', [ImportExportController::class, 'batch'])->name('batch');
 
-// Route::get('export', [ImportExportController::class, 'export'])->name('export');
-// Route::get('download', [ImportExportController::class, 'upload'])->name('export.upload');
+Route::get('export', [ImportExportController::class, 'export'])->name('export');
+Route::get('download', [ImportExportController::class, 'upload'])->name('export.upload');
